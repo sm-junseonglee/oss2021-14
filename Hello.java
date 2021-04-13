01640f25c09c856bc616e227fc3efaa699a1a8a5
Leejunseong <js2017243098@gmail.com>
2021-04-13 14:17:12 +0900

    Upda2 Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
-	System.out.prinntln("2021-04-13");
+	System.out.println("2021.04.13");
+	int a = 0;
+	System.out.println(a);
     }
 }
